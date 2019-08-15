3b12d1f844206d4d4623c5c16b46c3eabb161fd0
Jim Thomason <jim@japanesewartuba.com>
2019-08-15 13:32:04 -0500

    jimt/gozer - fixed vinz spelling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Zuul from "./components/Zuul"
-import vinzklortho from "./middleware/vinzclortho"
+import vinzclortho from "./middleware/vinzclortho"
 
 export * from "./utils/deepDiffDebug"
 export * from "./utils/deepWalkDebug"
 export * from "./utils/sanitizeObject"
 
-export { Zuul, vinzklortho }
+export { Zuul, vinzclortho }
